c445edba73a9d1c6a4c24d194ba459e054f817c2
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-01-21 21:59:45 -0300

    Tentei fazer uma urna

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-console.log("Hello World");
+function clicouUm() {
+    document.getElementById("numeroMonitor").innerHTML += 1;
+    return voto = + "1";
+}
+function clicouDois() {
+    document.getElementById("numeroMonitor").innerHTML += 2;
+    return voto = + "2";
+}
+function clicouTres() {
+    document.getElementById("numeroMonitor").innerHTML += 3;
+    return voto = + "3";
+}
+function clicouQuatro() {
+    document.getElementById("numeroMonitor").innerHTML += 4;
+    return voto = + "4";
+}
+function clicouCinco() {
+    document.getElementById("numeroMonitor").innerHTML += 5;
+    return voto = + "5";
+}
+function clicouSeis() {
+    document.getElementById("numeroMonitor").innerHTML += 6;
+    return voto = + "6";
+}
+function clicouSete() {
+    document.getElementById("numeroMonitor").innerHTML += 7;
+    return voto = + "7";
+}
+function clicouOito() {
+    document.getElementById("numeroMonitor").innerHTML += 8;
+    return voto = + "8";
+}
+function clicouNove() {
+    document.getElementById("numeroMonitor").innerHTML += 9;
+    return voto = + "9";
+}
+function clicouDez() {
+    document.getElementById("numeroMonitor").innerHTML += 0;
+    return voto = + "0";
+}
+
+var voto;
+
+console.log(voto);
