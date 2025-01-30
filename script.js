c7c69d54e093214385178671fe6aefa59e3d1e81
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-01-30 18:01:05 -0300

    Calcula F para C

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function calculaPerimetroArea() {
-    const raio = Number(document.getElementById("raio").value);
-    var perimetro = 2 * 3.14 * raio;
-    var area = 3.14 * (raio * raio);
-    document.getElementById("perimetro").innerHTML = "Perímetro: " + perimetro;
-    document.getElementById("area").innerHTML = "Área: " + area;
-
+function calculaFParaC() {
+    const f = Number(document.getElementById("f").value);
+    const c = 5 / 9 * (f - 32);
+    document.getElementById("resposta").innerHTML = "Celsius: " + c;
 }
