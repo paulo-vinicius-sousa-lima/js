30494e7fe0230812157f81b6f02bf6927f72da72
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-01-30 17:18:59 -0300

    Calculadora de área e perímetro de circulo

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function calculaMedia() {
-    const n1 = Number(document.getElementById("n1").value);
-    const n2 = Number(document.getElementById("n2").value);
-    var resultado = (n1 + n2) / 2;
-    document.getElementById("resultado").innerHTML = resultado;
+function calculaPerimetroArea() {
+    const raio = Number(document.getElementById("raio").value);
+    var perimetro = 2 * 3.14 * raio;
+    var area = 3.14 * (raio * raio);
+    document.getElementById("perimetro").innerHTML = "Perímetro: " + perimetro;
+    document.getElementById("area").innerHTML = "Área: " + area;
+
 }
