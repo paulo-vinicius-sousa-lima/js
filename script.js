55ec4658f963f17bb62c35a13526846fa952386a
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-01-30 18:18:43 -0300

    Exercício de lógica

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function calculaFParaC() {
-    const f = Number(document.getElementById("f").value);
-    const c = 5 / 9 * (f - 32);
-    document.getElementById("resposta").innerHTML = "Celsius: " + c;
+function verLogica() {
+    const a = Number(document.getElementById("a").value);
+    const b = Number(document.getElementById("b").value);
+    const c = Number(document.getElementById("c").value);
+
+    const resposta1 = a == b;
+    const resposta2 = a > b;
+    const resposta3 = a >= b;
+    const resposta4 = a < c;
+    const resposta5 = c != b;
+
+    document.getElementById("resposta1").innerHTML = "a =  b ? " + resposta1;
+    document.getElementById("resposta2").innerHTML = "a >  b ? " + resposta2;
+    document.getElementById("resposta3").innerHTML = "a >= b ? " + resposta3;
+    document.getElementById("resposta4").innerHTML = "a <  c ? " + resposta4;
+    document.getElementById("resposta5").innerHTML = "c != b ? " + resposta5;
 }
