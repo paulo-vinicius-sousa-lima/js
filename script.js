ef153143f6b7a43765484052128ecfe3288282c2
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-02-01 15:35:13 -0300

    Ver que tipo de triângulo é

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ function verLogica() {
     const b = Number(document.getElementById("b").value);
     const c = Number(document.getElementById("c").value);
 
-    const resposta1 = a == b;
-    const resposta2 = a > b;
-    const resposta3 = a >= b;
-    const resposta4 = a < c;
-    const resposta5 = c != b;
+    if ((a + b) > c && (a + c) > b && (b + c) > a) {
+        alert("Pode ser um triângulo.");
+        if (a == b && b == c) {
+            alert("Triângulo equilátero.");
+        }
+        else if (a == b || b == c || c == a) {
+            alert("Triângulo isóceles.");
+        }
+        else if (a != b && b != c && c != a) {
+            alert("Triângulo Escaleno.");
+        }
+    }
+    else {
+        alert("Não pode ser um triângulo!")
+    }
 
-    document.getElementById("resposta1").innerHTML = "a =  b ? " + resposta1;
-    document.getElementById("resposta2").innerHTML = "a >  b ? " + resposta2;
-    document.getElementById("resposta3").innerHTML = "a >= b ? " + resposta3;
-    document.getElementById("resposta4").innerHTML = "a <  c ? " + resposta4;
-    document.getElementById("resposta5").innerHTML = "c != b ? " + resposta5;
+
 }
