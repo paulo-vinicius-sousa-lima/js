4e56114c879b4cda0ab328447bb48ca38b42e8ba
Paulo Vinícius Sousa Lima <pauloviniciussousalima15@gmail.com>
2025-08-04 14:00:50 -0300

    Entrada de dados via teclado

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Digite seu nome: ", function(nome) {
+  console.log("Olá, " + nome + "!");
+  rl.close();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Length
+
+/*
 var vetor = new Array(20);
 
 console.log("O tamanho desse vetor é " + vetor.length);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*\
 
 // Array com números aleatórios
 
